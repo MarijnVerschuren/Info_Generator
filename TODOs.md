70e2d3871db598572444c488562f9d20422c0a10
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-10-25 15:47:05 +0200

    image serializer

--- TODOs.md
+++ TODOs.md
@@ -1,4 +1,7 @@
+# TODO
 analyze the image from thispersondoesnotexist.com
 and base the generated person of of it.
 https://www.geeksforgeeks.org/age-and-gender-detection-using-opencv-in-python/
  - gen.py@344
+
+improve the serializer to include profile picture
--- main.py
+++ main.py
@@ -1,16 +1,62 @@
+import numpy as np
+import base64
+import json
 import cv2
+import sys
+import os
 
 from info_gen import *
 
 
+image_dir = os.path.join(os.path.dirname(__file__), "images")
+
 if __name__ == "__main__":
-	info = info_gen()
+	info = info_gen(min_age=18, max_age=40)
 	password = info.password_gen
 	image = info.image_gen
 
-	pic = next(image)
-	print(next(info), next(password), pic.tobytes(), sep="\n\n")
-	cv2.imwrite("img.jpeg", pic)
+	with open("people.json", "r") as file:
+		people = json.load(file, cls=info_decoder)
+		file.close()
 
-# print(image.tobytes)
+	if "load" in sys.argv:
+		for person in people:
+			print(
+				person["person"],
+				f"password: {person['password']}",
+				sep="\n", end="\n\n"
+			)
+
+			cv2.imwrite(
+				os.path.join(image_dir, person["person"].name + ".jpeg"),
+				cv2.imdecode(
+					np.frombuffer(
+						base64.b64decode(
+							person["profile-picture"].encode("ascii")
+						),
+						np.uint8
+					),
+					cv2.IMREAD_COLOR
+				)
+			)
+
+	else:
+		image_data = base64.b64encode(
+			cv2.imencode(
+				".jpeg",
+				next(image)
+			)[1]
+		).decode('ascii')
+
+		people.append({
+			"person": next(info),
+			"password": next(password),
+			"profile-picture": image_data
+		})
+
+		with open("people.json", "w") as file:
+			json.dump(people, file, cls=info_encoder, indent=4)
+			file.close()
+
+
 # TODO: analize photo and base person of of it
